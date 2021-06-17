d6ca808907ed4f9f035ce6fc9e9b396aaf733dca
bidaffonso <bidaffondo@gmail.com>
2021-06-17 15:13:02 -0400

    Ranking page redirection button implementation

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from './pages/Login';
 import Game from './pages/Game';
 import Config from './pages/Config';
 import Feedback from './pages/Feedback';
+import Ranking from './pages/Ranking';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/config" component={ Config } />
       <Route path="/feedback" component={ Feedback } />
       <Route path="/game" component={ Game } />
+      <Route path="/ranking" component={ Ranking } />
       <Route exact path="/" component={ Login } />
     </Switch>
   );
