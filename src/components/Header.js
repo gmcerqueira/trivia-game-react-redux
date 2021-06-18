dda683383f3afc601ac5180257b4f6b6e000627f
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-06-18 20:58:44 -0300

    css header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import '../styles/Header.css';
 
 class Header extends Component {
   render() {
     const { name, gravatarEmail, score } = this.props;
     // const { points, timer, stopTimer, endGame, currentQuestion } = this.state;
     return (
-      <header>
-        <img
-          src={ `https://www.gravatar.com/avatar/${gravatarEmail}` }
-          alt=""
-          data-testid="header-profile-picture"
-        />
-        <p data-testid="header-player-name">{name}</p>
-        <p data-testid="header-score">{score}</p>
-      </header>
+      <div className="header-component">
+        <header className="header-container">
+          <div className="header-profile">
+            <img
+              className="header-profile-img"
+              src={ `https://www.gravatar.com/avatar/${gravatarEmail}` }
+              alt=""
+              data-testid="header-profile-picture"
+            />
+            <p data-testid="header-player-name">{name}</p>
+          </div>
+          <p data-testid="header-score">{`Score : ${score}`}</p>
+        </header>
+      </div>
     );
   }
 }
