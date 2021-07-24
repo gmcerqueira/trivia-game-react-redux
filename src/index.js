63d27234e89651c9b6f2169d41a9b7abd13f9f22
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-07-24 19:21:58 -0300

    gh pages update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import './index.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import App from './App';
 import store from './store';
 import * as serviceWorker from './serviceWorker';
