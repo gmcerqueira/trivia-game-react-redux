b92a73d33895c077086ceb53a37528dbfc74192b
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-07-24 19:35:18 -0300

    Updates

---
+++ precache-manifest.a4a4970022de616b51f19a229aef56f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b22c20418a1862a43c46f73acdc4d4e8",
+    "url": "/trivia-game-react-redux/index.html"
+  },
+  {
+    "revision": "d25c69c99825b4dd84aa",
+    "url": "/trivia-game-react-redux/static/css/main.e092d3f0.chunk.css"
+  },
+  {
+    "revision": "f003fff91f76d605f329",
+    "url": "/trivia-game-react-redux/static/js/2.6d420cb8.chunk.js"
+  },
+  {
+    "revision": "3efe9ddf98e7373a4a69e33015a2cf17",
+    "url": "/trivia-game-react-redux/static/js/2.6d420cb8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d25c69c99825b4dd84aa",
+    "url": "/trivia-game-react-redux/static/js/main.44977143.chunk.js"
+  },
+  {
+    "revision": "09a2e62062143ca5b6fd",
+    "url": "/trivia-game-react-redux/static/js/runtime-main.4a16c783.js"
+  }
+]);
